e51952fe5f0c4f05e4f812c127e9056fcbb49a7f
ilikecinepol <molotkov.m.a@gmail.com>
2021-10-14 19:01:40 +0300

    add context

--- products/views.py
+++ products/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'Geekshop',
+        'header': 'Интернет-магазин GeekShop',
+        'user': 'Иван Иванов',
+    }
+    return render(request, 'products/index.html', context)
+
 
 def products(request):
+    context = {
+        'title': 'Geekshop',
+        'header': 'Интернет-магазин GeekShop',
+        'user': 'Иван Иванов',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas', 'price': 6090},
+            {'name': 'Черный рюкзак Nike Heritage', 'price': 2340},
+            {'name': 'Черные туфли на платформе с 3 парами люверсов', 'price': 13590},
+        ],
+        'is_promotion' : 1,
+    }
     return render(request, 'products/products.html')
